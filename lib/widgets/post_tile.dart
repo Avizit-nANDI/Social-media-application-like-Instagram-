16d06f8208632d7541f7e48505ebfdaeb9a67823
AVIZIT NANDI <68993054+Avizit-nANDI@users.noreply.github.com>
2024-09-30 04:22:06 -0600

    Add files via upload

---
+++ lib/widgets/post_tile.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+//we dont use it
+
+class PostTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Post Tile");
+  }
+}
